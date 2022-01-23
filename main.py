e8fbd5c1dcbd08cc61b1d25b89463ac96a7827db
akash028 <akash.bhapkar028@gmail.com>
2022-01-22 21:20:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 # main.py: Driver program to test machine learning implementations on various datasets.
-#
-# Based on skeleton code by CSCI-B 551 Fall 2021 Course Staff
-#
-# Feel free to edit this driver program file in order to test other datasets or change specific parameters. When
-# grading, we will use our own driver program and test cases to test your machine learning implementations and functions
-# made from scratch.
 
 import sys
 
@@ -25,97 +19,6 @@ from sklearn.exceptions import ConvergenceWarning
 
 from k_nearest_neighbors import KNearestNeighbors
 from multilayer_perceptron import MultilayerPerceptron
-
-
-def test_knn():
-    print('Testing K-Nearest Neighbors Classification...')
-    param_dict = {'n_neighbors': [3, 5, 7, 9, 11], 'weights': ['uniform', 'distance'], 'metric': ['l1', 'l2']}
-    params_list = [dict(zip(param_dict.keys(), v)) for v in product(*param_dict.values())]
-    params_len = len(params_list)
-
-    knn_iris_results = pd.DataFrame(columns = ['  Model',
-                                               '  # of Neighbors',
-                                               '  Weight Function',
-                                               '  Distance Metric',
-                                               '  Accuracy Score'])
-
-    for i, params in enumerate(params_list):
-        sys.stdout.write('\r')
-        sys.stdout.write(f" - Iris Dataset Progress:   [{'=' * int(60 * (i + 1) / params_len):60s}]"
-                         f' {int(100 * (i + 1) / params_len)}%')
-        sys.stdout.flush()
-
-        sklearn_model = KNeighborsClassifier(n_neighbors = params['n_neighbors'],
-                                             weights = params['weights'],
-                                             algorithm = 'brute',
-                                             p = 1 if params['metric'] == 'l1' else 2)
-        sklearn_model.fit(X_train_i, y_train_i)
-        y_pred = sklearn_model.predict(X_test_i)
-
-        knn_iris_results.loc[len(knn_iris_results)] = ['Sklearn',
-                                                       params['n_neighbors'],
-                                                       params['weights'],
-                                                       params['metric'],
-                                                       round(accuracy_score(y_test_i, y_pred), 3)]
-
-        my_model = KNearestNeighbors(params['n_neighbors'], params['weights'], params['metric'])
-        my_model.fit(X_train_i, y_train_i)
-        y_pred = my_model.predict(X_test_i)
-
-        knn_iris_results.loc[len(knn_iris_results)] = ['My Model',
-                                                       params['n_neighbors'],
-                                                       params['weights'],
-                                                       params['metric'],
-                                                       round(accuracy_score(y_test_i, y_pred), 3)]
-
-        knn_iris_results.loc[len(knn_iris_results)] = ['', '', '', '', '']
-
-    print()
-
-    knn_digits_results = pd.DataFrame(columns = ['  Model ',
-                                                 '  # of Neighbors',
-                                                 '  Weight Function',
-                                                 '  Distance Metric',
-                                                 '  Accuracy Score'])
-
-    for i, params in enumerate(params_list):
-        sys.stdout.write('\r')
-        sys.stdout.write(f" - Digits Dataset Progress: [{'=' * int(60 * (i + 1) / params_len):60s}]"
-                         f' {int(100 * (i + 1) / params_len)}%')
-        sys.stdout.flush()
-
-        sklearn_model = KNeighborsClassifier(n_neighbors = params['n_neighbors'],
-                                             weights = params['weights'],
-                                             algorithm = 'brute',
-                                             p = 1 if params['metric'] == 'l1' else 2)
-        sklearn_model.fit(X_train_d, y_train_d)
-        y_pred = sklearn_model.predict(X_test_d)
-
-        knn_digits_results.loc[len(knn_digits_results)] = ['Sklearn',
-                                                           params['n_neighbors'],
-                                                           params['weights'],
-                                                           params['metric'],
-                                                           round(accuracy_score(y_test_d, y_pred), 3)]
-
-        my_model = KNearestNeighbors(params['n_neighbors'], params['weights'], params['metric'])
-        my_model.fit(X_train_d, y_train_d)
-        y_pred = my_model.predict(X_test_d)
-
-        knn_digits_results.loc[len(knn_digits_results)] = ['My Model',
-                                                           params['n_neighbors'],
-                                                           params['weights'],
-                                                           params['metric'],
-                                                           round(accuracy_score(y_test_d, y_pred), 3)]
-
-        knn_digits_results.loc[len(knn_digits_results)] = ['', '', '', '', '']
-
-    print('\n')
-
-    print('Exporting KNN Results to HTML Files...\n')
-    knn_iris_results.to_html('knn_iris_results.html')
-    knn_digits_results.to_html('knn_digits_results.html')
-
-    print('Done Testing K-Nearest Neighbors Classification!\n')
 
 
 def test_mlp():
@@ -259,13 +162,7 @@ if __name__ == '__main__':
     X_test_i = sc_i.transform(X_test_i)
     X_train_d = sc_d.fit_transform(X_train_d)
     X_test_d = sc_d.transform(X_test_d)
-
-    if sys.argv[1] == 'knn':
-        test_knn()
-    elif sys.argv[1] == 'mlp':
-        test_mlp()
-    else:
-        test_knn()
-        test_mlp()
-
+    
+    test_mlp()
+    
     print('Program Finished! Exiting the Program...')
