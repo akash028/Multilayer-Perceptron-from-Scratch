6024f12c5ff48814b459e3f2d5dddd9539b4a9d7
akash028 <akash.bhapkar028@gmail.com>
2022-01-22 21:18:47 -0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,149 @@
+# utils.py: Utility file for implementing helpful utility functions used by the ML algorithms.
+#
+# Submitted by: [enter your full name here] -- [enter your IU username here]
+#
+# Based on skeleton code by CSCI-B 551 Fall 2021 Course Staff
+
+import numpy as np
+import math
+
+def euclidean_distance(x1, x2):
+    """
+    Computes and returns the Euclidean distance between two vectors.
+
+    Args:
+        x1: A numpy array of shape (n_features,).
+        x2: A numpy array of shape (n_features,).
+    """
+    return np.sqrt(np.sum(pow(np.subtract(x1, x2), 2)))
+    #raise NotImplementedError('This function must be implemented by the student.')
+
+
+def manhattan_distance(x1, x2):
+    """
+    Computes and returns the Manhattan distance between two vectors.
+
+    Args:
+        x1: A numpy array of shape (n_features,).
+        x2: A numpy array of shape (n_features,).
+    """
+    #l = len(x1) if len(x1) > len(x2) else len(x2)
+
+    return np.sum(abs(np.subtract(x1, x2)))
+
+    #raise NotImplementedError('This function must be implemented by the student.')
+
+
+def identity(x, derivative = False):
+    """
+    Computes and returns the identity activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    if derivative:
+        return 1
+    return x
+    #raise NotImplementedError('This function must be implemented by the student.')
+
+
+def sigmoid(x, derivative = False):
+    """
+    Computes and returns the sigmoid (logistic) activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    if derivative:
+        sig = 1/(1 + np.exp(-x))
+        return sig*(1 - sig)
+    return 1/(1 + np.exp(-x))
+
+    #raise NotImplementedError('This function must be implemented by the student.')
+
+
+def tanh(x, derivative = False):
+    """
+    Computes and returns the hyperbolic tangent activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    if derivative:
+        return 1.0 - np.tanh(x)**2
+    return np.tanh(x)
+    #raise NotImplementedError('This function must be implemented by the student.')
+
+
+def relu(x, derivative = False):
+    """
+    Computes and returns the rectified linear unit activation function of the given input data x. If derivative = True,
+    the derivative of the activation function is returned instead.
+
+    Args:
+        x: A numpy array of shape (n_samples, n_hidden).
+        derivative: A boolean representing whether or not the derivative of the function should be returned instead.
+    """
+    if derivative:
+        return np.where(x > 0.0, 1, 0)
+    return np.where(x > 0.0, x, 0)
+    #raise NotImplementedError('This function must be implemented by the student.')
+
+
+def softmax(x, derivative = False):
+    x = np.clip(x, -1e100, 1e100)
+    if not derivative:
+        c = np.max(x, axis = 1, keepdims = True)
+        return np.exp(x - c - np.log(np.sum(np.exp(x - c), axis = 1, keepdims = True)))
+    else:
+        return softmax(x) * (1 - softmax(x))
+
+
+def cross_entropy(y, p):
+    """
+    Computes and returns the cross-entropy loss, defined as the negative log-likelihood of a logistic model that returns
+    p probabilities for its true class labels y.
+
+    Args:
+        y:
+            A numpy array of shape (n_samples, n_outputs) representing the one-hot encoded target class values for the
+            input data used when fitting the model.
+        p:
+            A numpy array of shape (n_samples, n_outputs) representing the predicted probabilities from the softmax
+            output activation function.
+    """
+
+    # Added 10^-7 when p is 0 to avoid RuntimeWarning of divide by 0
+    return -np.mean(y * np.log(p + pow(10, -7)))
+
+    #raise NotImplementedError('This function must be implemented by the student.')
+
+
+def one_hot_encoding(y):
+    """
+    Converts a vector y of categorical target class values into a one-hot numeric array using one-hot encoding: one-hot
+    encoding creates new binary-valued columns, each of which indicate the presence of each possible value from the
+    original data.
+
+    Args:
+        y: A numpy array of shape (n_samples,) representing the target class values for each sample in the input data.
+
+    Returns:
+        A numpy array of shape (n_samples, n_outputs) representing the one-hot encoded target class values for the input
+        data. n_outputs is equal to the number of unique categorical class values in the numpy array y.
+    """
+    l = np.unique(y)    #number of features
+    en = []
+    for i, val in enumerate(y):
+        c = [0] * len(l)
+        c[val] = 1
+        en.append(c)
+    encoded = np.array(en)
+    return encoded
+    #raise NotImplementedError('This function must be implemented by the student.')
